d8b1a9d778c657d4017b90abecba9379a41dee09
Kevin Song <song_zj@hotmail.com>
2024-10-16 00:55:00 -0400

    initial commit, still in progress

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const cors = require('cors');
+app.use(express.json());
+app.use(express.static('dist'));
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(cors());
+app.use(requestLogger);
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint);
+
+app.listen(PORT, () => { 
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './App.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '/logo.png';
+
+const App = () => {
+  const navItems = [
+    { label: 'Home', href: '/' },
+    { label: 'About', href: '/about' },
+    { label: 'Services', href: '/services' },
+    { label: 'Contact', href: '/contact' },
+  ];
+
+  return (
+    <>
+      <Navbar bg="white" fixed="top">
+        <Container>
+          <Navbar.Brand href="#home" className="d-flex align-items-center">
+            <img
+              src={logo}
+              alt="Logo"
+              width="75" // Adjust width as needed
+              height="75" // Adjust height as needed
+              className="d-inline-block align-top"
+            />{' '}
+            <span className="ms-2">Penn Ghost Lab</span>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <section className="ch-header">
+        <div className="ch-header-content">
+          <h1>Chameleon</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
