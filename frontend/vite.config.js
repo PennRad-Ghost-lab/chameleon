afb789de544272a364f278b0a739ad54327dfb9e
Ebubechukwu Enwerem <ebubechukwuenwerem@Ebubechukwus-MacBook-Pro.local>
2024-11-29 13:59:06 -0500

    error in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/chameleon/',
-  plugins: [react()],
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist'
+  },
+  plugins: [react()]
 })
+
