54ee70e2e09485a9859ec8069b9c78b04a6dba2f
Ebubechukwu Enwerem <ebubechukwuenwerem@Ebubechukwus-MacBook-Pro.local>
2024-11-29 13:54:08 -0500

    updated page url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/chameleon/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    }
-  },
-  // base: "/chameleon/",
 })
